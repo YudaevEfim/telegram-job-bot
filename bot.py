853648f54281d56a1a90b73044ae6b671091fc68
YudaevEfim <tvoifima@gmail.com>
2026-03-16 13:22:07 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет, Ефим. Я твой бот для поиска работы.")
+
+@bot.message_handler(commands=['jobs'])
+def jobs(message):
+    bot.send_message(message.chat.id, "Пока вакансий нет. Скоро начну искать.")
+
+bot.infinity_polling()
